ab03ec1b68b240d83786dffc8f158f9084c68c62
pellejuul <pelle.juul@tuta.io>
2018-04-08 20:17:23 +0200

    Added note frequency calculation function

--- library/utils.cpp
+++ library/utils.cpp
@@ -17,3 +17,9 @@ float randNum()
 {
     return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
 }
+
+float noteToFreq(int note)
+{
+    float c = std::pow(2, 1.0 / 12.0);
+    return std::pow(c, note - 69) * 440;
+}
--- library/utils.h
+++ library/utils.h
@@ -1,15 +1,21 @@
 /** Converts from seconds to number samples.
     
-    @param  seconds         The number of seconds to convert to samples.
+    @param  seconds     The number of seconds to convert to samples.
 */
 int samples(float seconds);
 
 /** Convertes from number of samples to seconds
 
-    @param  samples         The number of samples to convert to seconds.
+    @param  samples     The number of samples to convert to seconds.
 */
 float seconds(int samples);
 
 /** Returns a random float in the range [0:1).
 */
 float randNum();
+
+/** Calculates the frequency of the given MIDI note number
+
+    @param  note        The MIDI note to calculate the frequency of.
+*/
+float noteToFreq(int note);
